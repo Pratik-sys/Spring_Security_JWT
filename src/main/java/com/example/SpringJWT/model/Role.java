2e5688eb30f7b81d8ce302bd92bab3f8a43f31a2
Pratik-sys <pithore.pratik@gmail.com>
2024-03-16 23:06:43 +0530

    implement user entity with UserDetails from spring security

---
+++ src/main/java/com/example/SpringJWT/model/Role.java
@@ -0,0 +1,6 @@
+package com.example.SpringJWT.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/example/SpringJWT/model/Users.java
+++ src/main/java/com/example/SpringJWT/model/Users.java
@@ -1,13 +1,16 @@
 package com.example.SpringJWT.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -15,11 +18,39 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class Users {
+public class Users implements UserDetails {
     @Id
     @GeneratedValue()
     private Integer id;
     private  String username;
     private  String email;
     private  String Password;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
