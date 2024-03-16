f19c1119e6998d2fc40f65b5a747057aaf7b5e8c
Pratik-sys <pithore.pratik@gmail.com>
2024-03-16 16:40:47 +0530

    Make user class entity

--- src/main/java/com/example/SpringJWT/model/Users.java
+++ src/main/java/com/example/SpringJWT/model/Users.java
@@ -1,4 +1,25 @@
 package com.example.SpringJWT.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class Users {
+    @Id
+    @GeneratedValue()
+    private Integer id;
+    private  String username;
+    private  String email;
+    private  String Password;
 }
--- src/main/java/com/example/SpringJWT/repository/UserRepository.java
+++ src/main/java/com/example/SpringJWT/repository/UserRepository.java
@@ -1,4 +1,5 @@
 package com.example.SpringJWT.repository;
 
 public interface UserRepository {
+
 }
