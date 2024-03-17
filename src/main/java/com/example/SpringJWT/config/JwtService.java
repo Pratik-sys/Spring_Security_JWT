d1fac8a44c0b1fa1ed4ebe59d7a48b2384a2373b
Pratik-sys <pithore.pratik@gmail.com>
2024-03-17 23:32:25 +0530

    Add few boolean functions to validate tokens

--- src/main/java/com/example/SpringJWT/config/JwtService.java
+++ src/main/java/com/example/SpringJWT/config/JwtService.java
@@ -44,4 +44,16 @@ public class JwtService {
                 .signWith(getPublicSigningKey(), Jwts.SIG.HS256)
                 .compact();
     }
+    public  boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return  extractClaim(token, Claims::getExpiration);
+    }
 }
