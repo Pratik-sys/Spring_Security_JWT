49d5a13cb53b615d6cb328ef130363ee1cf262ff
Pratik-sys <pithore.pratik@gmail.com>
2024-03-17 19:45:24 +0530

    extract a particular claim

--- src/main/java/com/example/SpringJWT/config/JwtService.java
+++ src/main/java/com/example/SpringJWT/config/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,6 +15,10 @@ public class JwtService {
     private static final String SECRETE_KEY = "38E7F356DE5EBD112343C881D6A5C";
     public String extractUsername(String token){
         return null;
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final  Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
     private Claims extractAllClaims(String token){
         return Jwts.parser()
