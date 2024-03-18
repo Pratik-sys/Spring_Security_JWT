a946d4a0a42488c5cfa6d9356c63b56fcdfe2141
Pratik-sys <pithore.pratik@gmail.com>
2024-03-18 11:45:04 +0530

    login and register controller

---
+++ src/main/java/com/example/SpringJWT/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.SpringJWT.controller;
+
+import com.example.SpringJWT.dto.AuthenticationResponse;
+import com.example.SpringJWT.dto.UserLogIn;
+import com.example.SpringJWT.dto.UserSignIn;
+import com.example.SpringJWT.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private  final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> registerUser(@RequestBody UserSignIn userSignIn){
+        return  ResponseEntity.ok().body(userService.registerUser(userSignIn));
+    }
+
+    @PostMapping("/login")
+    public  ResponseEntity<AuthenticationResponse> loginUser(@RequestBody UserLogIn userLogIn){
+        return  ResponseEntity.ok().body(userService.loginInuser(userLogIn));
+    }
+
+}
--- src/main/java/com/example/SpringJWT/service/UserService.java
+++ src/main/java/com/example/SpringJWT/service/UserService.java
@@ -1,9 +1,15 @@
 package com.example.SpringJWT.service;
 
 import com.example.SpringJWT.config.JwtService;
+import com.example.SpringJWT.dto.AuthenticationResponse;
+import com.example.SpringJWT.dto.UserLogIn;
+import com.example.SpringJWT.dto.UserSignIn;
+import com.example.SpringJWT.model.Role;
+import com.example.SpringJWT.model.Users;
 import com.example.SpringJWT.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +22,32 @@ public class UserService {
     private  final AuthenticationManager authManager;
 
 
+    public AuthenticationResponse registerUser(UserSignIn userSignIn){
+        var user = Users.builder()
+                .username(userSignIn.getUsername())
+                .email(userSignIn.getEmail())
+                .Password(passwordEncoder.encode(userSignIn.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+    public  AuthenticationResponse loginInuser(UserLogIn userLogIn){
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userLogIn.getUsername(),
+                        userLogIn.getPassword()
+
+                )
+        );
+        var user = userRepository.findByUsername(userLogIn.getUsername()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
 
 }
